ef22162b5d1799d67c9ac1c485ab8a938bfaae2a
Kailey09 <kcoop1323@gmail.com>
2022-04-12 11:30:04 -0400

    added inqurier

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name,github);
 
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err =>{
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err =>{
+//     if (err) throw err;
 
-    console.log('Portfilio complete! Checkout index.html to see the output!');
-});
+//     console.log('Portfilio complete! Checkout index.html to see the output!');
+// });
 
 
-
+inquirer 
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
 
