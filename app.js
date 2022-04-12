84744f93f168075f08446c16a4b5f75d7e0b2e88
Kailey09 <kcoop1323@gmail.com>
2022-04-12 11:11:46 -0400

    added page-temolate

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
 
 
-const printProfileData = (profileDataArr) => {
-    // This...
-    for (let  i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log("=============");
+fs.writeFile('index.html', generatePage(name, github), err =>{
+    if (err) throw err;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-
-};
+    console.log('Portfilio complete! Checkout index.html to see the output!');
+});
 
 
-printProfileData(profileDataArgs);
+
+
+
